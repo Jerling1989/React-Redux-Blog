6bcd78c6e05c655f6cba585bd1f64a3e80ee815b
Jacob <jerling1989@gmail.com>
2018-05-27 17:57:06 -0400

    setup index route

---
+++ src/components/posts_index.js
@@ -0,0 +1,16 @@
+// IMPORT OBJECTS AND METHODS
+import React, { Component } from 'react';
+
+// CREATE POSTSINDEX CLASS COMPONENT
+class PostsIndex extends Component {
+	render() {
+		return (
+			<div>
+				Posts Index
+			</div>
+		);
+	}
+}
+
+// EXPORT POSTSINDEX COMPONENT
+export default PostsIndex;
