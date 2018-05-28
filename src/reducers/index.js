9c1d3de5765dc7d77054af08dd20108c21724dae
Jacob <jerling1989@gmail.com>
2018-05-28 00:24:12 -0400

    install redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 // IMPORT OBJECTS AND METHODS
 import { combineReducers } from 'redux';
 // IMPORT REDUCERS
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 // CREATE ROOTREDUCER
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 // EXPORT ROOTREDUCER
