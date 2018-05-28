d5c56ea92e6ab2179a3884bb63efbe5605041e82
Jacob <jerling1989@gmail.com>
2018-05-28 00:04:50 -0400

    react router bug fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 // IMPORT COMPONENTS AND REDUCERS
 import reducers from './reducers';
@@ -17,8 +17,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     	<div>
-    		<Route path="/" component={PostsIndex} />
-    		<Route path="/posts/new" component={PostsNew} />
+    		<Switch>
+    			<Route path="/posts/new" component={PostsNew} />
+	    		<Route path="/" component={PostsIndex} />
+    		</Switch>
     	</div>
     </BrowserRouter>
   </Provider>
