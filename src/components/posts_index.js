c273ad9aad2d928c850484f8095b650ae18b6415
Jacob <jerling1989@gmail.com>
2018-05-27 20:40:53 -0400

    rendering list of posts in DOM

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,8 @@
 // IMPORT OBJECTS AND METHODS
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+// IMPORT PACKAGES
+import _ from 'lodash';
 // IMPORT ACTION CREATOR
 import { fetchPosts } from '../actions';
 
@@ -10,17 +12,34 @@ class PostsIndex extends Component {
 	componentDidMount() {
 		this.props.fetchPosts();
 	}
-
+	// RENDER POSTS HELPER FUNCTION
+	renderPosts() {
+		return _.map(this.props.posts, post => {
+			return (
+				<li className="list-group-item" key={post.id}>
+					{post.title}
+				</li>
+			);
+		});
+	}
 	// REACT RENDOR METHOD
 	render() {
 		return (
 			<div>
-				Posts Index
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 			</div>
 		);
 	}
 }
 
+// ADD APPLICATION LEVEL STATE(REDUX) TO COMPONENT
+function mapsStateToProps(state) {
+	return { posts: state.posts };
+}
+
 // EXPORT POSTSINDEX COMPONENT
 // ATTACH ACTION CREATOR WITH PROPS TO COMPONENT
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapsStateToProps, { fetchPosts })(PostsIndex);
