582d870b65e4d0984b669521b7b65a39bf15741d
Jacob <jerling1989@gmail.com>
2018-05-29 02:06:22 -0400

    selecting from ownProps

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,8 +1,17 @@
 // IMPORT OBJECTS AND METHODS
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// IMPORT ACTION CREATOR
+import { fetchPost } from '../actions';
 
 // CREATE POSTSSHOW CLASS COMPONENT
 class PostsShow extends Component {
+	// LIFECYCLE TO RUN ONCE COMPONENT RENDERS
+	componentDidMount() {
+		const { id } = this.props.match.params;
+		this.props.fetchPost(id);
+	}
+	// REACT RENDER METHOD
 	render() {
 		return (
 			<div>
@@ -12,5 +21,11 @@ class PostsShow extends Component {
 	}
 }
 
+// BIND APP STATE PROPS TO COMPONENT
+function mapStateToProps({ posts }, ownProps) {
+	// RETURN THE POST WITH MATCHING ID
+	return { posts: posts[ownProps.match.params.id] };
+}
+
 // EXPORT POSTSSHOW COMPONENT
-export default PostsShow;
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
