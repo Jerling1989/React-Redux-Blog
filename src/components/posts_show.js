1b49b5780264a769098fd903ea80ddad5a22d377
Jacob <jerling1989@gmail.com>
2018-05-29 01:12:08 -0400

    create PostsShow component

---
+++ src/components/posts_show.js
@@ -0,0 +1,16 @@
+// IMPORT OBJECTS AND METHODS
+import React, { Component } from 'react';
+
+// CREATE POSTSSHOW CLASS COMPONENT
+class PostsShow extends Component {
+	render() {
+		return (
+			<div>
+				Posts Show!
+			</div>
+		);
+	}
+}
+
+// EXPORT POSTSSHOW COMPONENT
+export default PostsShow;
