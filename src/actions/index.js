9671e63ef67a3a4f4d9763a30c722d477cdb774a
Jacob <jerling1989@gmail.com>
2018-05-29 01:28:56 -0400

    receiving new posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,11 +4,12 @@ import axios from 'axios';
 // EXPORT TYPE VARIABLE
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
+export const FETCH_POST = 'fetch_post';
 // CREATE API URL AND KEY VARIABLSE
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=jacob1221';
 
-// CREATE ACTION FUNCTION
+// CREATE FETCHPOSTS (ALL) ACTION CREATOR
 export function fetchPosts() {
 	// USE AXIOS FOR AJAX GET REQUEST
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -31,3 +32,26 @@ export function createPost(values, callback) {
 		payload: request
 	};
 }
+
+// CREATE FETCHPOST (SPECIFIC) ACTION CREATOR
+export function fetchPost(id) {
+	// USE AXIOS FOR AJAX GET REQUEST
+	const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+	// RETURN ACTION OBJECT WITH TYPE AND REQUEST PROMISE AS PAYLOAD
+	return {
+		type: FETCH_POST,
+		payload: request
+	};
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,18 @@
 // IMPORT PACKAGES
 import _ from 'lodash';
 // IMPORT TYPE VARIABLES
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 // CREATE POSTSREDUCER
 export default function(state = {}, action) {
 	switch (action.type) {
+
+		case FETCH_POST:
+			// const post = action.payload.data;
+			// const newState = { ...state };
+			// newState[post.id] = post;
+			// return newState;
+			return { ...state, [action.payload.data.id]: action.payload.data }
 
 		case FETCH_POSTS:
 			// TURN POSTS ARRAY INTO OBJECT CONTAINING POSTS WITH ID KEYS SET
